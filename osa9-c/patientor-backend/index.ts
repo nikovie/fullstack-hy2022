546e2dc01c45eb060cea3ce15249a8e06ec8c318
Niko Vierikko <niko.vierikko@beanbakers.fi>
2022-06-14 11:55:03 +0300

    Exercise 9.9

--- osa9-c/patientor-backend/index.ts
+++ osa9-c/patientor-backend/index.ts
@@ -1,10 +1,21 @@
 import express = require('express');
+import cors from 'cors';
+
 const app = express();
+
+const allowedOrigins = ['http://localhost:3000'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins,
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors(options));
 app.use(express.json());
 
-const PORT = 3000;
+const PORT = 3001;
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   console.log('ping');
   res.send('pong');
 });
